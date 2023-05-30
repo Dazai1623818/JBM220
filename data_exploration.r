86de3ed37fbd2d535b67a925abd36e70488159b6
Dazai <c.c.e.lee@student.tue.nl>
2023-05-30 12:07:28 +0200

    added PCR

--- data_exploration.r
+++ data_exploration.r
@@ -1,11 +1,38 @@
+# install and load libraries
 install.packages("ggplot2")
 install.packages("Hmisc")
-libraries <- c("rstudioapi", "Hmisc", "ggplot2")
+install.packages("tidyverse")
+install.packages("rstudioapi")
+libraries <- c("rstudioapi", "Hmisc", "ggplot2", "tidyverse")
 lapply(libraries, library, character.only = TRUE)
 
+# set wd and load data
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 data <- read.csv("data.csv", sep = ";")
 
-numerical <- data[, (125):ncol(data)]
+# select variables and plot corr
+# numerical <- data[, (125):ncol(data)]
+numerical <- data[, (5):ncol(data)]
 summary(numerical)
 windows();pairs(numerical)
+
+#calculate principal components https://www.statology.org/principal-components-analysis-in-r/
+results <- prcomp(numerical, scale = TRUE)
+
+#reverse the signs
+results$rotation <- -1*results$rotation
+
+#display principal components
+results$rotation
+results$x <- -1*results$x
+biplot(results, scale = 0)
+#calculate total variance explained by each principal component
+var_explained = results$sdev^2 / sum(results$sdev^2)
+
+#create scree plot
+windows();qplot(c(1:125), var_explained) + 
+  geom_line() + 
+  xlab("Principal Component") + 
+  ylab("Variance Explained") +
+  ggtitle("Scree Plot") +
+  ylim(0, 1)
