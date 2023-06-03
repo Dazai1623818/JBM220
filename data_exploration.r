0e412f9d3aa3b805aac6c0edb4e5afcf254833ae
Dazai <c.c.e.lee@student.tue.nl>
2023-06-03 14:20:23 +0200

    PCA reveals that scan_type is easily distinguishable

--- data_exploration.r
+++ data_exploration.r
@@ -3,36 +3,52 @@ install.packages("ggplot2")
 install.packages("Hmisc")
 install.packages("tidyverse")
 install.packages("rstudioapi")
-libraries <- c("rstudioapi", "Hmisc", "ggplot2", "tidyverse")
+install.packages("randomForest")
+install.packages("caret")
+install.packages("gplots")
+libraries <- c("rstudioapi", "Hmisc", "ggplot2", "tidyverse", "randomForest", "caret", "gplots")
 lapply(libraries, library, character.only = TRUE)
+
+
+set.seed(42)
 
 # set wd and load data
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 data <- read.csv("data.csv", sep = ";")
 
-# select variables and plot corr
-# numerical <- data[, (125):ncol(data)]
+# remove trailing whitespace in rows
+data$Production_system <- trimws(data$Production_system)
+# extract numerical data
 numerical <- data[, (5):ncol(data)]
 summary(numerical)
-windows();pairs(numerical)
+mean(cor(numerical))
 
-#calculate principal components https://www.statology.org/principal-components-analysis-in-r/
-results <- prcomp(numerical, scale = TRUE)
+# Apply PCA
+results <- prcomp(numerical, scale = FALSE)
+# we can see that 99% is explained by first 2 components
+summary(results)
+plot(results, type="l")
 
-#reverse the signs
-results$rotation <- -1*results$rotation
+# we can roughly see three different classes
+windows();biplot(results, scale = 0, xlim = c(-4, 4), ylim = c(-4, 4), cex = 0.25)
 
-#display principal components
-results$rotation
-results$x <- -1*results$x
-biplot(results, scale = 0)
-#calculate total variance explained by each principal component
-var_explained = results$sdev^2 / sum(results$sdev^2)
+# we further investigate and color them by variables
+vis = cbind(data, results$x[, 1:2])
 
-#create scree plot
-windows();qplot(c(1:125), var_explained) + 
-  geom_line() + 
-  xlab("Principal Component") + 
-  ylab("Variance Explained") +
-  ggtitle("Scree Plot") +
-  ylim(0, 1)
+windows()
+ggplot(vis, aes(PC1, PC2, col = Scan_type, fill = Scan_type)) +
+  stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) +
+  geom_point(shape = 21, col = "black") +
+  coord_cartesian(xlim = c(-4, 4), ylim = c(-4, 4))
+
+windows()
+ggplot(vis, aes(PC1, PC2, col = Freshness, fill = Freshness)) +
+  stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) +
+  geom_point(shape = 21, col = "black") +
+  coord_cartesian(xlim = c(-4, 4), ylim = c(-4, 4))
+
+windows()
+ggplot(vis, aes(PC1, PC2, col = Production_system, fill = Production_system)) +
+  stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) +
+  geom_point(shape = 21, col = "black") +
+  coord_cartesian(xlim = c(-4, 4), ylim = c(-4, 4))
